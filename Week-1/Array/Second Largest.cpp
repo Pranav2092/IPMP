30f84ac3b558252dcb880e15fa8d800a61ad6ed8
Pranav Anand Prakash Sharma <130064033+Pranav2092@users.noreply.github.com>
2025-01-11 15:51:06 +0530

    Create Second Largest.cpp

---
+++ Week-1/Array/Second Largest.cpp
@@ -0,0 +1,56 @@
+Question : https://www.geeksforgeeks.org/problems/second-largest3735/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=second-largest
+
+Solution :
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int getSecondLargest(vector<int> &arr) {
+        // Code Here
+        int maxval = -1;
+        for(auto val : arr){
+            maxval = max(maxval,val);
+        }
+        int semaxval = -1;
+        for(auto val: arr){
+            if(val < maxval){
+                semaxval = max(semaxval,val);
+            }
+        }
+        return semaxval;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.getSecondLargest(arr);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
